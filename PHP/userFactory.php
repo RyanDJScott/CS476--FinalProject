5cf08bb37e7eae0c59f81b85b621cb2e8faf4323
Ryan Scott <67915358+RyanDJScott@users.noreply.github.com>
2021-02-18 15:57:03 -0600

    Added user classes/variables/methods
    -Added the abstract user class, and the community user/admin user class that implements it.
    -Added member variables to both classes.
    -Added member function prototypes to both classes.
    
    Working on #21 .

--- PHP/userFactory.php
+++ PHP/userFactory.php
@@ -19,4 +19,106 @@ class adminUserFactory extends userFactory {
         return new adminUser;
     }
 }
+
+abstract class user {
+    //Base user functionalities here
+    //Both users implement these functions
+    abstract function setInfo($info);
+    abstract function getInfo();
+    abstract function setUID($UID);
+    abstract function getUID();
+    abstract function displayProfile();
+    abstract function editProfile($newInfo);
+    abstract function deleteAccount();
+    abstract function displayDashboard();
+    abstract function addTGE($TG);
+    abstract function leaveReview($review);
+    abstract function flagReview($revNum);
+
+    //Admin functionalities here
+    //Admin: implements these functions
+    //Comm. Users: Block the user from using these functions
+    abstract function deleteUser($UID);
+    abstract function reviewFlag($review);
+    abstract function reviewTGE($TGE);
+    abstract function promoteUser($UID);
+}
+
+class communityUser extends user {
+    //Member variables
+    private $userInfo;
+    private $userDashboard;
+    private $UID;
+
+    //Member functions
+    abstract function setInfo($info) {}
+
+    abstract function getInfo() {}
+
+    abstract function setUID($UID) {}
+
+    abstract function getUID() {}
+
+    abstract function displayProfile() {}
+
+    abstract function editProfile($newInfo) {}
+
+    abstract function deleteAccount() {}
+
+    abstract function displayDashboard() {}
+
+    abstract function addTGE($TG) {}
+
+    abstract function leaveReview($review) {}
+
+    abstract function flagReview($revNum) {}
+
+
+    abstract function deleteUser($UID) {}
+
+    abstract function reviewFlag($review) {}
+
+    abstract function reviewTGE($TGE) {}
+
+    abstract function promoteUser($UID) {}
+}
+
+class adminUser extends user {
+    //Member variables
+    private $userInfo;
+    private $userDashboard;
+    private $UID;
+
+    //Member functions
+    abstract function setInfo($info) {}
+
+    abstract function getInfo() {}
+
+    abstract function setUID($UID) {}
+
+    abstract function getUID() {}
+
+    abstract function displayProfile() {}
+
+    abstract function editProfile($newInfo) {}
+
+    abstract function deleteAccount() {}
+
+    abstract function displayDashboard() {}
+
+    abstract function addTGE($TG) {}
+
+    abstract function leaveReview($review) {}
+
+    abstract function flagReview($revNum) {}
+
+
+    abstract function deleteUser($UID) {}
+
+    abstract function reviewFlag($review) {}
+
+    abstract function reviewTGE($TGE) {}
+
+    abstract function promoteUser($UID) {}
+}
 ?>
