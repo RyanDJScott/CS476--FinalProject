6058f5309c219c071923a12ff27ecbf6150e0537
Ryan Scott <67915358+RyanDJScott@users.noreply.github.com>
2021-02-18 19:21:35 -0600

    Prototyped member functions
    Prototypes for all member functions added.
    
    Added $gameReviews to hold all of the reviews for the game.
    
    Working on #23

--- PHP/TGE.php
+++ PHP/TGE.php
@@ -4,8 +4,21 @@ class TGE {
     //Member Variables
     private $entryInfo = NULL;
     private $images = NULL;
+    private $gameReviews = NULL;
 
     //Member Functions
-    
+    public function __construct($gameTitle) {}
+
+    public function setEntryInfo($gameInfo) {}
+
+    public function getEntryInfo() {}
+
+    public function setImages($imageURLs) {}
+
+    public function getImages($imageURLs) {}
+
+    public function displayTGE() {}
+
+    public function displayTGECard() {}
 }
 ?>
