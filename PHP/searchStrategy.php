471bb324b6fce72434f382749c62e2e39e6e90f5
Ryan Scott <67915358+RyanDJScott@users.noreply.github.com>
2021-02-18 18:04:45 -0600

    Implemented searchStrategy interface
    Implemented the searchStrategy interface with two public functions: getResults and displayResults.
    
    Working on #22

---
+++ PHP/searchStrategy.php
@@ -0,0 +1,19 @@
+<?php
+
+class Search {
+    private $searchType;
+    private $query;
+    private $searchStrategy = NULL;
+    
+    public function __construct ($searchOption, $searchQuery) {}
+
+    public function publishResults ($query) {}
+}
+
+interface searchStrategy {
+    public function getResults($query);
+    public function displayResults($dbReturn);
+}
+
+
+?>
