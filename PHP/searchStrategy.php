8fdee62eeef58d72e835dcb8ee91325100c4d19f
Ryan Scott <67915358+RyanDJScott@users.noreply.github.com>
2021-02-18 18:53:07 -0600

    Implemented gameSearch class
    Added gameSearch class with two overrode implemented functions.
    
    Working on #22

--- PHP/searchStrategy.php
+++ PHP/searchStrategy.php
@@ -15,8 +15,14 @@ interface searchStrategy {
     public function displayResults($dbReturn);
 }
 
-class userSearch extends searchStrategy {
+class userSearch implements searchStrategy {
     public function getResults($query) {}
+
+    public function displayResults($dbReturn) {}
+}
+
+class gameSearch implements searchStrategy {
+    public function getResults($querry) {}
 
     public function displayResults($dbReturn) {}
 }
