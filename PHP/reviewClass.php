007c764db814e2282a821650213eb55c007410ca
Ryan Scott <67915358+RyanDJScott@users.noreply.github.com>
2021-02-18 19:53:17 -0600

    Added member functions
    Added member functions to the Review class.
    
    Working on #24

--- PHP/reviewClass.php
+++ PHP/reviewClass.php
@@ -5,5 +5,16 @@
         private $flagStatus = NULL;
 
         //Member functions
+        public function __construct($gameTitle) {}
+
+        public function setRevInfo($newReviewInfo) {}
+
+        public function getRevInfo() {}
+
+        public function setFlag($flagVal) {}
+
+        public function getFlag() {}
+
+        public function displayReview() {}
     }
 ?>
