5e56b20bff56d42effea079fa5832a7b88105ba7
Ryan Scott <67915358+RyanDJScott@users.noreply.github.com>
2021-03-08 11:12:18 -0600

    Defined submitTGE class
    -Defined the member variables and functions of the submitTGE class.

--- PHP/submitTGEScripts.php
+++ PHP/submitTGEScripts.php
@@ -1,3 +1,44 @@
 <?php
+//Include database class for connection
+include_once(__DIR__ . '/database.php');
+
+class submitTGE {
+    //Member variables
+    private $UID = NULL;
+    private $dateSubmitted = NULL;
+    private $numPlayers = NULL;
+    private $ageRating = NULL;
+    private $playTime = NULL;
+    private $description = NULL;
+    private $company = NULL;
+    private $expansions = NULL;
+    private $images = NULL;
+
+    //Database connection variables
+    private $db = NULL;
+    private $dbConnect = NULL;
+
+    //Member functions
+
+    public function __construct($userID) {}
+
+    private function valDate () {}
+
+    private function valNumPlayers () {}
+
+    private function valAgeRating () {}
+
+    private function valPlayTime () {}
+
+    private function valDescription () {}
+
+    private function valCompany () {}
+
+    private function valExpansions () {}
+
+    private function uploadImages () {}
+
+    public function submitForm() {}
+}
 
 ?>
