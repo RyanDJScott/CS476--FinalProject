9b54d52b97f7785649f716f77d3ae9203c489581
Ryan Scott <67915358+RyanDJScott@users.noreply.github.com>
2021-03-10 19:53:30 -0600

    Added display function, modified viewProfile.php
    -Added a displayViewProfile function. It does NOT take a user; I didn't want to instantiate a user incase this lead to some sort of security flaw.
    -Removed the HTML from viewProfile.php
    -Added GET method information to grab userID, make display object.

--- viewProfile.php
+++ viewProfile.php
@@ -8,17 +8,14 @@
 
     //Continue the session
     session_start();
-
-    //If sent by the GET method, grab user ID and create objects
+    
+    //If you were sent here by the GET method; grab the userID
     if (isset($_GET["UID"]) && $_GET["UID"] > 0) {
         $userID = $_GET["UID"];
 
-        //
-    //Create a display object for the page
-    $display = new Display();
-
-    
-    
+        //Create a display object
+        $display = new Display();
+    }
 ?>
 <!DOCTYPE html>
 <HTML lang="en">
@@ -47,58 +44,9 @@
     <!-- Dashboard header image -->
     <div class="dashboardHeader">
         <img src="dependencies/dashboardImage.png" class="dashboardHeaderImage" alt="Welcome to Queen City's Gambit!" />
-        <div class="headerImageMessage">
-            USER INFO HERE
-        </div>
     </div>
 
-    <h1>User Information</h1>
-    <!-- Main Area Container Which holds all -->
-    <div class="mainContainer">
-
-        <!-- user information container -->
-        <div class="userInformation">
-
-            <!-- image and basic information container -->
-            <div class="userInformationLeft">
-
-                <!-- image container -->
-                <div class="userProfileImage">
-                    <img src="userImage.png" alt="profile picture">
-                </div>
-
-                <!-- information container -->
-                <div class="simpleUserInfo">
-                    <p>Ryan Scott</p>
-                    <p>23/04/2001</p>
-                    <p>ryanscott@geekagogo.ca</p>
-                </div>
-            </div>
-
-            <!-- biography, favourites, and edit profile container-->
-            <div class="userInformationRight">
-
-                <!-- biography -->
-                <div class="userBiography">
-                    <p>My name is Ryan DJ Scott and I enjoy tabletop games!
-                        My name is Ryan DJ Scott and I enjoy tabletop games!
-                        My name is Ryan DJ Scott and I enjoy tabletop games!
-                        My name is Ryan DJ Scott and I enjoy tabletop games!
-                        My name is Ryan DJ Scott and I enjoy tabletop games!
-                    </p>
-                </div>
-
-                <!-- right side information-->
-                <div class="favouritesInfo">
-                    <p>Favourite Game: GeekAGone</p>
-                    <p>Type: Strategy</p>
-                    <p>Time Playing Game: 10+ years</p>
-                </div>
-
-            </div>
-
-        </div>
-    </div>
+    <?php $display->displayViewProfile($userID); ?>    
 
 </body>
 
