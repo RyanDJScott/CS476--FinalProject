313cb8e9b2431f68d02277bf6f9480b97eb75726
Ryan Scott <67915358+RyanDJScott@users.noreply.github.com>
2021-03-14 15:28:10 -0600

    Fixed bugs in reviewFlag and reviewTGE
    -Pages were still attempting to load even with the redirect; added logic to only display the page after all login checks were made.

--- reviewTGE.php
+++ reviewTGE.php
@@ -9,53 +9,52 @@ include_once(__DIR__ . '/PHP/display.php');
 session_start();
 
 //Check to see if they are logged in, if not redirect to login page
-if (!isset($_SESSION["UID"]) && !is_object($_SESSION["userObj"])) 
+if (!isset($_SESSION["UID"]) && !isset($_SESSION["userObj"])) 
     header("Location: login.php");
-
 //Check to see if they are an administrator; redirect to dashboard if not
-if (!(is_a($_SESSION["userObj"], 'adminUser')))
+else if (is_object($_SESSION["userObj"]) && !is_a($_SESSION["userObj"], 'adminUser'))
     header("Location: dashboard.php");
-//Get the game title from the GET method, instantiate new TGE/Display
-if (isset($_GET["gameTitle"]) && strlen($_GET["gameTitle"]) > 0) {
-    //Get the game title, create a new TGE object from it, then create display object
-    $gameTitle = $_GET["gameTitle"];
-    $thisGame = new TGE($gameTitle);
-    $display = new Display();
-}
+else {
+    //Get the game title from the GET method, instantiate new TGE/Display
+    if (isset($_GET["gameTitle"]) && strlen($_GET["gameTitle"]) > 0) {
+        //Get the game title, create a new TGE object from it, then create display object
+        $gameTitle = $_GET["gameTitle"];
+        $thisGame = new TGE($gameTitle);
+        $display = new Display();
+    }
 
-//Execute admin functions depending on what was pressed
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    //Retrieve the gameTitle and send it to the admin functions
-    $gameTitle = $_POST["gameTitle"];
+    //Execute admin functions depending on what was pressed
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        //Retrieve the gameTitle and send it to the admin functions
+        $gameTitle = $_POST["gameTitle"];
 
-    //If accept was pressed, set 1/reason. If reject was pressed, set 0/reason. Reason field must be filled in
-    if (isset($_POST["approveTGE"]) && $_POST["approveTGE"] === "APPROVE" && strlen($_POST["gameFeedback"]) != 0)
-        $updateResult = $_SESSION["userObj"]->setTGEStatus($gameTitle, 1, $_POST["gameFeedback"]);
-    else if (isset($_POST["rejectTGE"]) && $_POST["rejectTGE"] === "REJECT" && strlen($_POST["gameFeedback"]) != 0)
-        $updateResult = $_SESSION["userObj"]->setTGEStatus($gameTitle, 0, $_POST["gameFeedback"]);
+        //If accept was pressed, set 1/reason. If reject was pressed, set 0/reason. Reason field must be filled in
+        if (isset($_POST["approveTGE"]) && $_POST["approveTGE"] === "APPROVE" && strlen($_POST["gameFeedback"]) != 0)
+            $updateResult = $_SESSION["userObj"]->setTGEStatus($gameTitle, 1, $_POST["gameFeedback"]);
+        else if (isset($_POST["rejectTGE"]) && $_POST["rejectTGE"] === "REJECT" && strlen($_POST["gameFeedback"]) != 0)
+            $updateResult = $_SESSION["userObj"]->setTGEStatus($gameTitle, 0, $_POST["gameFeedback"]);
 
-    //If the update worked, send the admin back to their dashboard. Otherwise post error
-    if ($updateResult)
-        header("Location: dashboard.php");
-    else if ($updateResult == FALSE && strlen($_POST["gameFeedback"]) == 0)
-        header("Location: reviewTGE.php?gameTitle=" . $gameTitle . "&error=fb_empty");
-    else
-        header("Location: reviewTGE.php?gameTitle=" . $gameTitle . "&error=st_error");
-}
+        //If the update worked, send the admin back to their dashboard. Otherwise post error
+        if ($updateResult)
+            header("Location: dashboard.php");
+        else if ($updateResult == FALSE && strlen($_POST["gameFeedback"]) == 0)
+            header("Location: reviewTGE.php?gameTitle=" . $gameTitle . "&error=fb_empty");
+        else
+            header("Location: reviewTGE.php?gameTitle=" . $gameTitle . "&error=st_error");
+    }
 
-//Error checking through GET method
-$errorMessage = "";
+    //Error checking through GET method
+    $errorMessage = "";
 
-//Throw error depending on error variable
-if (isset($_GET["error"])) {
-    if ($_GET["error"] === "st_error")
-        $errorMessage = "There was an error updating the status of this game. Please try again.";
-    
-    if($_GET["error"] === "fb_empty")
-        $errorMessage = "You must provide a reason for your decision.";
-}
+    //Throw error depending on error variable
+    if (isset($_GET["error"])) {
+        if ($_GET["error"] === "st_error")
+            $errorMessage = "There was an error updating the status of this game. Please try again.";
+        
+        if($_GET["error"] === "fb_empty")
+            $errorMessage = "You must provide a reason for your decision.";
+    }
 ?>
-
 <!DOCTYPE html>
 <!-- very similar to review flagged review -->
 <html>
@@ -126,3 +125,6 @@ if (isset($_GET["error"])) {
 </body>
 
 </html>
+<?php 
+}
+?>
